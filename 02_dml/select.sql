cac4ed15139a6dd78818f5665b47d2572085c8ee
edujeeho <edujeeho@gmail.com>
2023-12-19 16:01:14 +0900

    Aggregate & Join

--- 02_dml/select.sql
+++ 02_dml/select.sql
@@ -52,24 +52,55 @@ SELECT * FROM user WHERE first_name = 'Zelda';
 SELECT * FROM user WHERE first_name LIKE 'A%';
 -- first_name이 A로 시작하는 사람들 중 이름이 4글자로 이루어진 사람들만 조회
 -- _ = 1개의 문자와 동일한 문자
+-- Alex, Adam, Acer, Asus
 SELECT * FROM user WHERE first_name LIKE 'A___';
 SELECT * FROM user WHERE age = '23';
 
+-- 전화번호가 `010-`으로 시작하는 데이터를 조회하는 SQL
+SELECT * FROM user WHERE phone LIKE '010%';
 
+-- 전화번호가 `010-`으로 시작하지 않는 데이터를 조회하는 SQL
+SELECT * FROM user WHERE phone NOT LIKE '010%';
 
+-- 성에 `'`이 포함된 이름을 조회하는 SQL
+SELECT * FROM user WHERE last_name LIKE '%''%';
 
+-- 국정이 미국(United States), 캐나다(Canada), 호추(Australia)
+-- 인 사람들만 조회하는 SQL
+SELECT * FROM user
+WHERE
+    country = 'United States'
+   OR country = 'Canada'
+   OR country = 'Australia';
 
+-- IN 사용
+SELECT * FROM user
+WHERE country IN ('United States', 'Canada', 'Australia');
 
+-- BETWEEN 사용
+SELECT first_name, last_name, age
+FROM user
+WHERE age BETWEEN 30 AND 39;
 
+-- LIMIT
+SELECT id, first_name, last_name
+FROM user
+LIMIT 20;
 
+SELECT id, first_name, last_name
+FROM user
+WHERE age > 30
+LIMIT 50;
 
+SELECT id, first_name, last_name
+FROM user
+WHERE age > 30
+ORDER BY age
+LIMIT 50;
 
-
-
-
-
-
-
-
-
+SELECT id, first_name, last_name
+FROM user
+WHERE age > 30
+ORDER BY age
+LIMIT 50 OFFSET 50;
 
